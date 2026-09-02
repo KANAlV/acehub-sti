9ab420ae2420350742795d236647b61c327c875a
KANAlV <editicroyedics@gmail.com>
2026-09-02 20:16:11 +0800

    feat(validation.ts) - made it impossible to do consecutive spaces using the functions

--- utils/validation.ts
+++ utils/validation.ts
@@ -4,25 +4,25 @@ export const filterEmail = (str: string): string =>
   str.replace(/[^a-zA-Z0-9._%+-@]/g, "");
 
 /**
- * Strips all non-alphabetic characters (a-zA-Z).
+ * Strips all non-alphabetic characters (a-zA-Z) and collapse consecutive spaces into one.
  */
 export const filterAlpha = (str: string): string =>
-  str.replace(/[^a-zA-Z ]/g, "");
+  str.replace(/[^a-zA-Z ]/g, "").replace(/\s+/g, " ");
 
 /**
- * Strips all non-alphanumeric characters (a-zA-Z0-9).
+ * Strips all non-alphanumeric characters (a-zA-Z0-9) and collapse consecutive spaces into one.
  */
 export const filterAlphanumeric = (str: string): string =>
-  str.replace(/[^a-zA-Z0-9 ]/g, "");
+  str.replace(/[^a-zA-Z0-9 ]/g, "").replace(/\s+/g, " ");
 
 /**
- * Strips all characters except letters and underscores (a-zA-Z_).
+ * Strips all characters except letters and underscores (a-zA-Z_) and collapse consecutive spaces into one.
  */
 export const filterAlphaUnderscore = (str: string): string =>
-  str.replace(/[^a-zA-Z_ ]/g, "");
+  str.replace(/[^a-zA-Z_ ]/g, "").replace(/\s+/g, " ");
 
 /**
- * Strips all non-alphanumeric characters (a-zA-Z0-9) and underscore.
+ * Strips all non-alphanumeric characters (a-zA-Z0-9) and underscore, and collapse consecutive spaces into one.
  */
 export const filterAlphanumericUnderscore = (str: string): string =>
-  str.replace(/[^a-zA-Z0-9_ ]/g, "");
+  str.replace(/[^a-zA-Z0-9_ ]/g, "").replace(/\s+/g, " ");
